07cdb35c1c4a218fa9a55a16799ffc996930bb75
ejdao <ejdao2022@gmail.com>
2026-02-24 16:14:06 -0500

    :bento: Recursos iniciales agregados

---
+++ libraries/layouts/src/lib/layouts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-layouts',
+  imports: [],
+  template: ` <p>layouts works!</p> `,
+  styles: ``,
+})
+export class Layouts {}
---
+++ libraries/layouts/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of layouts
+ */
+
+export * from './lib/layouts';
