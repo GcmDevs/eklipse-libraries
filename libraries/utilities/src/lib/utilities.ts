5dca747b32d4dc14542851e0b60418a3dda2cdb2
ejdao <ejdao2022@gmail.com>
2026-02-24 15:07:11 -0500

    :bento: Recursos iniciales agregados

---
+++ libraries/utilities/src/lib/utilities.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-utilities',
+  imports: [],
+  template: ` <p>utilities works!</p> `,
+  styles: ``,
+})
+export class Utilities {}
